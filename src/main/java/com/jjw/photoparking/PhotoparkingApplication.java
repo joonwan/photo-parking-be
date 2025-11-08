9be667ba216b1eccc608b7d359df5d9a244240c1
joonwan1024 <joownan1024@naver.com>
2025-11-08 23:59:59 +0900

    init project

---
+++ src/main/java/com/jjw/photoparking/PhotoparkingApplication.java
@@ -0,0 +1,13 @@
+package com.jjw.photoparking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhotoparkingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhotoparkingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jjw/photoparking/PhotoparkingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jjw.photoparking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhotoparkingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
