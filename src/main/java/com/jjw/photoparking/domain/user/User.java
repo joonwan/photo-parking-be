4c1cf53b46db5936793f8f237e089c194dd64388
joonwan1024 <joownan1024@naver.com>
2025-11-09 22:09:00 +0900

    feat: user entity 생성

---
+++ src/main/java/com/jjw/photoparking/domain/user/User.java
@@ -0,0 +1,24 @@
+package com.jjw.photoparking.domain.user;
+
+import com.jjw.photoparking.domain.BaseEntity;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Table(name = "users")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class User extends BaseEntity {
+
+    @Id
+    @Column(name = "user_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Long kakaoId;
+
+    private String username;
+
+}
