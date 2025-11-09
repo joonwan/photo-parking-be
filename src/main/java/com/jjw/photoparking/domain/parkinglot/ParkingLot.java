4e15df5c4fe35c639d409eeee17568af7724f361
joonwan1024 <joownan1024@naver.com>
2025-11-09 22:23:20 +0900

    feat: 주차장 도메인 생성

---
+++ src/main/java/com/jjw/photoparking/domain/parkinglot/ParkingLot.java
@@ -0,0 +1,38 @@
+package com.jjw.photoparking.domain.parkinglot;
+
+import com.jjw.photoparking.domain.user.User;
+import jakarta.persistence.*;
+
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Table(name = "parking_lots")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class ParkingLot {
+
+    @Id
+    @Column(name = "parking_lot_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "parking_log_name", nullable = false, length = 255)
+    private String parkingLotName;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @Builder
+    private ParkingLot(String parkingLotName) {
+        this.parkingLotName = parkingLotName;
+    }
+
+    public void initializeUser(User user) {
+        this.user = user;
+        user.addParkingLot(this);
+    }
+}
--- src/main/java/com/jjw/photoparking/domain/user/User.java
+++ src/main/java/com/jjw/photoparking/domain/user/User.java
@@ -1,10 +1,15 @@
 package com.jjw.photoparking.domain.user;
 
 import com.jjw.photoparking.domain.BaseEntity;
+import com.jjw.photoparking.domain.parkinglot.ParkingLot;
 import jakarta.persistence.*;
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Getter
@@ -21,4 +26,22 @@ public class User extends BaseEntity {
 
     private String username;
 
+    @OneToMany(mappedBy = "user")
+    private List<ParkingLot> parkingLots = new ArrayList<>();
+
+    @Builder
+    private User(Long kakaoId, String username) {
+        this.kakaoId = kakaoId;
+        this.username = username;
+    }
+
+    public void addParkingLot(ParkingLot newParkingLot) {
+
+        /*todo
+        * 중복 등록 방지용 가드 필요
+        */
+
+        this.parkingLots.add(newParkingLot);
+        newParkingLot.initializeUser(this);
+    }
 }
